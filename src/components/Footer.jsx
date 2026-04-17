9a7d06f7b22970b73373fcebb14584b8e87de8f4
Jamiul-kawsar <jamiul.kawsar35@gmail.com>
2026-04-17 10:56:55 +0600

    fix the footer name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
         {/* Bottom Bar */}
         <div className="border-t border-gray-800 pt-8 flex flex-col md:flex-row justify-between items-center text-sm text-gray-500">
           <p>
-            &copy; {currentYear} Jamiul Kadir. All rights reserved.
+            &copy; {currentYear} Jamiul Kawsar. All rights reserved.
           </p>
           <div className="flex gap-6 mt-4 md:mt-0">
             <a href="#" className="hover:text-gray-300 transition duration-300">
